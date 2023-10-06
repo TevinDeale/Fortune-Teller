e82fbd2e46e23100efcfff11bcf63c66729f7853
Tevin Deale <103233487+TevinDeale@users.noreply.github.com>
2023-10-05 20:25:43 -0500

    added file reader to read txt file line by line

--- fortune-teller.js
+++ fortune-teller.js
@@ -5,8 +5,10 @@ const fs = require('fs');
 // Reading fortunes from text file and creating an array named messages.
 const messages = fs.readFileSync('fortunes.txt', 'utf-8').split('\n');
 
+// Generates a random number up to the number of fortunes in the array messages.
 let random = Math.ceil(Math.random()*messages.length);
 
+// Prints fortune to console.
 console.log(messages[random]);
 
 
