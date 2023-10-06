93e7b991f3d923e8324c94d3302ea59296d6e4a1
Tevin Deale <103233487+TevinDeale@users.noreply.github.com>
2023-10-05 20:23:58 -0500

    Added a txt file with fortunes.

--- fortune-teller.js
+++ fortune-teller.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+// Requiring fs module in which
+// readFile function is defined.
+const fs = require('fs');
+
+// Reading fortunes from text file and creating an array named messages.
+const messages = fs.readFileSync('fortunes.txt', 'utf-8').split('\n');
+
+let random = Math.ceil(Math.random()*messages.length);
+
+console.log(messages[random]);
+
+
+
+
+
+
