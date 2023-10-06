77da928c2e70e0b8822ea33de6b9a94a6d65352f
Tevin Deale <103233487+TevinDeale@users.noreply.github.com>
2023-10-05 20:40:12 -0500

    added ascii image and improved styling

--- fortune-teller.js
+++ fortune-teller.js
@@ -2,6 +2,9 @@
 // readFile function is defined.
 const fs = require('fs');
 
+// importing the figlet module
+const figlet = require('figlet');
+
 // Reading fortunes from text file and creating an array named messages.
 const messages = fs.readFileSync('fortunes.txt', 'utf-8').split('\n');
 
@@ -9,7 +12,19 @@ const messages = fs.readFileSync('fortunes.txt', 'utf-8').split('\n');
 let random = Math.ceil(Math.random()*messages.length);
 
 // Prints fortune to console.
-console.log(messages[random]);
+figlet("Fortunes", function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(''); console.log('');
+    console.log(data);
+    console.log(' ');
+    console.log(messages[random]);
+    console.log(''); console.log('');
+  });
+
 
 
 
